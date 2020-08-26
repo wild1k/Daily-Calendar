396740085585c1625ca92495b4024c60713449bb
wild1k <cssndrchmbrln@gmail.com>
2020-08-26 14:36:18 -0700

    got the calender to render on the page dynamically now and appropriatiately color coded to past, present of future time slot

--- Develop/script.js
+++ Develop/script.js
@@ -7,4 +7,32 @@ var currentDay = document.getElementById('currentDay')
 currentDay.innerHTML= time
 
 
+var userInput = [];
+
+
+function dailyEvents() {
+  const timeArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+  let html = "";
+
+  timeArray.forEach((time, i) => {
+    const hr = i + 9;
+    const nw = moment().hour();
+    const color = (hr === nw) ? "present" :
+      (hr > nw) ? "future" :
+        "past";
+
+    html += `<div class="row">
+        <div class="col-md-1 hour d-flex justify-content-center align-items-center">${time}</div> 
+        <input class='user-event-input col-md-10 event-input-block ${color}' value="${localStorage.getItem(`${hr}`) || ""}" id="${time}Row"> 
+        <div class="col-md-1 saveBtn" data-hour=${hr}><i class="fas fa-save pl-4 pt-4 " aria-hidden="true"></i></div> 
+    </div>`
+
+  });
+
+  $("#calendarContain").append(html);
+
+
+}
+dailyEvents();
+
 
